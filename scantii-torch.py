c8dacdb143fdebe1a8b9cae6817df184cdc1264e
scantii <pscale@gmail.com>
2025-05-01 22:10:07 -0400

    added main commands to scantii-torch.py

--- scantii-torch.py
+++ scantii-torch.py
@@ -1,16 +1,24 @@
 # checkpoint: meta-llama/Llama-3.2-1B-Instruct
-# tune run lora_finetune_single_device --config custom_config.yaml 
+# 
+# llama3_2/1B_lora_single_device
 # llama3_2/1B_qlora_single_device
 # 
-# tune cp <recipe> custom_config.yaml
+### tune cp <recipe> custom_type_config.yaml
 # 
-### tune run <recipe> --config <config> epochs=1
+### tune validate <config>
+#
+### tune run <recipe> --config <config>
 # 
-# tune run lora_finetune_single_device --config llama3_2/1B_qlora_single_device epochs=1
-# 
-# 
-# tune validate custom_config.yaml
-# 
+#
+# tune cp llama3_2/1B_lora_single_device dev/configs/custom_lora_config.yaml
+# tune cp llama3_2/1B_qlora_single_device dev/configs/custom_qlora_config.yaml
+#
+# tune validate dev/configs/custom_lora_config.yaml
+# tune validate dev/configs/custom_qlora_config.yaml
+#
+# tune run lora_finetune_single_device --config dev/configs/custom_lora_config.yaml
+# tune run lora_finetune_single_device --config dev/configs/custom_qlora_config.yaml
+#
 
 import torch
 x = torch.rand(5, 3)
@@ -18,7 +26,3 @@ y = torch.cuda.is_available()
 z = torch.cuda.is_bf16_supported()
 print(x)
 print(f'cuda = {y} bf16 = {z}')
-
-# llama3_2/1B_qlora_single_device
-
-# tune cp llama3_2/1B_lora_single_device custom_lora_config.yaml
